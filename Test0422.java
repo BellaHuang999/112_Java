0a926c228b42e0d567ec8bec32c63e40a1145228
Bella <ella159go@gmail.com>
2023-04-23 12:35:44 +0800

    first commit

---
+++ Test0422.java
@@ -0,0 +1,19 @@
+package com.bella;
+
+import java.util.Scanner;
+
+public class Test0422 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int[] arr = {1, 2, 3};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        System.out.println("test git");
+
+
+
+    }
+}
