d7b1f9a71dc4910352e21611a241c6c8cff0351e
Bella <ella159go@gmail.com>
2023-04-23 13:19:58 +0800

    commit again

--- Test0423.java
+++ Test0423.java
@@ -1,9 +1,14 @@
 package com.bella;
 
+import java.util.Scanner;
+
 public class Test0423 {
     public static void main(String[] args) {
         System.out.println("測試0423");
         System.out.println("測試0423-2");
+        System.out.println("測試0423-3");
+        System.out.println("helloWorld");
+
 
     }
 }
