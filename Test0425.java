ea0265155bb68fc17ed207a3e974e3d8bf8ed76a
Bella <ella159go@gmail.com>
2023-04-24 19:01:54 +0800

    last test

--- Test0425.java
+++ Test0425.java
@@ -3,19 +3,7 @@ package com.bella;
 public class Test0425 {
     public static void main(String[] args) {
         System.out.println("測試0423");
-        System.out.println("測試0423-2");
-        System.out.println("測試0423-3");
-        System.out.println("helloWorld");
-        System.out.println("helloWorld2133234");
-        System.out.println("helloWorld214");
-        System.out.println("helloWorld21asd4");
-        System.out.println("helloWorld21asd4");
-        System.out.println("helloWorld21asd4");
-        System.out.println("helloWorld21asd4");
-        System.out.println("helloWorld21asd4");
-        System.out.println("helloWorld21asd4");
-        System.out.println("helloWorld21asd4");
-        System.out.println("helloWor最後asd4");
+
 
 
 
